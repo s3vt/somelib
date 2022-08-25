4907abee48f7e4c726b5bc4cd7fa3004e0cf71b2
s3vt <111678562+s3vt@users.noreply.github.com>
2022-08-25 14:08:53 +0530

    v1.0.1

--- lib.go
+++ lib.go
@@ -3,5 +3,9 @@ package somelib
 import "fmt"
 
 func Version() {
-	fmt.Println("I am version 1.0.0")
+	fmt.Println("I am version 1.0.1")
+}
+
+func NewFunction() {
+	fmt.Println("I am new function added in version 1.0.1")
 }
